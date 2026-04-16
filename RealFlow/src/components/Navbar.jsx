a156e485e1f7e74f841fbf7845792e1e5ee13cf8
anilyadav-netizen <anilyadav@maxifysolution.com>
2026-04-16 16:56:06 +0530

    hello

---
+++ RealFlow/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <titel className="text-red-600">Hello Mr Anil</titel>
+                </div>
+                <div className='grid grid-cols-3 gap-10'>
+                    <h3>hello</h3>
+                    <h3>hello</h3>
+                    <h3>hello</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
